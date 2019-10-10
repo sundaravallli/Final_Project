77198f19807abb9b0c0967a15f4aad7a09172d11
sundaravalli <sundaravalli1@gmail.com>
2019-10-09 18:26:00 -0700

    Update changes in Final_Independent

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 
 $(document).ready(function() {
     $("form").submit(function() {
-        var number_chosen=[1,2,3,4,5,6,7,8];
-        for (var index = 1; index <=number_chosen.length; index += 1) {
+        var number_chosen=[ ];
+        var size = parseInt($("input#user").val());
+        for (var index = 1; index <=size; index += 1) {
     if (index % 3===0) {
         document.write("ping");
     }
